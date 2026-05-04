a02834ae5f339e7e41299b10ac2e445a509d6d8e
Sorokovsky <Sorokovskys@ukr.net>
2026-05-04 16:57:38 +0300

    refactor: pupils

---
+++ src/main/java/pro/sorokovsky/schoolmanagerbackend/entity/PupilEntity.java
@@ -0,0 +1,29 @@
+package pro.sorokovsky.schoolmanagerbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(schema = "dbo", name = "Pupils")
+@SuperBuilder
+@PrimaryKeyJoinColumn(name = "UserId")
+public class PupilEntity extends UserEntity {
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "FatherId")
+    private ParentEntity father;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "MotherId")
+    private ParentEntity mother;
+
+    @Column(name = "ExtraInformation", nullable = false, length = 1000)
+    private String extraInformation;
+}
