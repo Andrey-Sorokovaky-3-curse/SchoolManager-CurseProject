ebdba6d9083e05c08f070b27a1aa3e12b372a415
Sorokovsky <Sorokovskys@ukr.net>
2026-05-05 10:22:34 +0300

    feature: docs

--- src/main/java/pro/sorokovsky/schoolmanagerbackend/controller/EmployeesController.java
+++ src/main/java/pro/sorokovsky/schoolmanagerbackend/controller/EmployeesController.java
@@ -1,14 +1,21 @@
 package pro.sorokovsky.schoolmanagerbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pro.sorokovsky.schoolmanagerbackend.contract.employee.GetEmployee;
-import pro.sorokovsky.schoolmanagerbackend.entity.EmployeeEntity;
 import pro.sorokovsky.schoolmanagerbackend.mapper.EmployeeMapper;
 import pro.sorokovsky.schoolmanagerbackend.service.EmployeesService;
 
@@ -23,6 +30,27 @@ public class EmployeesController {
     private final EmployeeMapper mapper;
 
     @GetMapping("by-position")
+    @Operation(summary = "Відділ кадрів", description = "Отримує працівників за назвою посади.")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Успішне отримання.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = GetEmployee.class)
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Неавторизований.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            ),
+    })
     public ResponseEntity<List<GetEmployee>> findByPosition(@RequestParam String name) {
         return ResponseEntity.ok().body(service.getByPosition(name).stream().map(mapper::toGet).toList());
     }
--- src/main/java/pro/sorokovsky/schoolmanagerbackend/controller/SubjectsController.java
+++ src/main/java/pro/sorokovsky/schoolmanagerbackend/controller/SubjectsController.java
@@ -1,6 +1,15 @@
 package pro.sorokovsky.schoolmanagerbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,12 +24,34 @@ import java.util.List;
 @RestController
 @RequestMapping("subjects")
 @RequiredArgsConstructor
+@Tag(name = "Предмети")
 public class SubjectsController {
     private final SubjectsService service;
     private final SubjectMapper mapper;
 
     @GetMapping("by-teacher/{teacherId:\\d+}")
-    public ResponseEntity<List<GetSubject>>  getByTeacher(@PathVariable Integer teacherId) {
+    @Operation(summary = "Предмети", description = "Отримує предмети за вчителем")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Успішне отримання.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            array = @ArraySchema(
+                                    schema = @Schema(implementation = GetSubject.class)
+                            )
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Неавторизований.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            ),
+    })
+    public ResponseEntity<List<GetSubject>> getByTeacher(@PathVariable Integer teacherId) {
         return ResponseEntity.ok(service.getByTeacher(teacherId).stream().map(mapper::toGet).toList());
     }
 }
