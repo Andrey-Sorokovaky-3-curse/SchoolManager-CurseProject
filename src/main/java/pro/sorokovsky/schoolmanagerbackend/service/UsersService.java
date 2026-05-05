5b3b79840cf93ee6cc28a32bce52eafc3cb81d8f
Sorokovskys <Sorokovskys@ukr.net>
2026-05-05 15:04:13 +0300

    fix: create user bug

--- src/main/java/pro/sorokovsky/schoolmanagerbackend/service/UsersService.java
+++ src/main/java/pro/sorokovsky/schoolmanagerbackend/service/UsersService.java
@@ -10,7 +10,6 @@ import org.springframework.stereotype.Service;
 import pro.sorokovsky.schoolmanagerbackend.contract.user.CreateUser;
 import pro.sorokovsky.schoolmanagerbackend.entity.Roles;
 import pro.sorokovsky.schoolmanagerbackend.entity.UserEntity;
-import pro.sorokovsky.schoolmanagerbackend.mapper.UserMapper;
 import pro.sorokovsky.schoolmanagerbackend.repository.UsersRepository;
 
 import java.util.Optional;
@@ -20,10 +19,9 @@ import java.util.Optional;
 public class UsersService implements UserDetailsService {
     private final UsersRepository repository;
     private final PasswordEncoder passwordEncoder;
-    private final UserMapper mapper;
 
     public UserEntity create(@NonNull CreateUser newUser) {
-        return UserEntity
+        return repository.save(UserEntity
                 .builder()
                 .login(newUser.login())
                 .password(passwordEncoder.encode(newUser.password()))
@@ -34,7 +32,7 @@ public class UsersService implements UserDetailsService {
                 .birthday(newUser.birthday().toLocalDate())
                 .gender(newUser.gender())
                 .address(newUser.address())
-                .build();
+                .build());
     }
 
     public Optional<UserEntity> getById(Integer id) {
