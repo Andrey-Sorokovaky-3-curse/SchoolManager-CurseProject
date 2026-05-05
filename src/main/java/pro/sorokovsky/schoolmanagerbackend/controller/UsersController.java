30cd7572cebe135b67e054b51fa9abdb5f9b0d3d
Sorokovskys <Sorokovskys@ukr.net>
2026-05-05 16:55:03 +0300

    feature: delete user

--- src/main/java/pro/sorokovsky/schoolmanagerbackend/controller/UsersController.java
+++ src/main/java/pro/sorokovsky/schoolmanagerbackend/controller/UsersController.java
@@ -7,21 +7,20 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pro.sorokovsky.schoolmanagerbackend.contract.classes.GetClass;
 import pro.sorokovsky.schoolmanagerbackend.contract.user.GetUser;
 import pro.sorokovsky.schoolmanagerbackend.contract.user.UpdateUser;
 import pro.sorokovsky.schoolmanagerbackend.entity.UserEntity;
 import pro.sorokovsky.schoolmanagerbackend.mapper.UserMapper;
+import pro.sorokovsky.schoolmanagerbackend.service.AuthorizationService;
 import pro.sorokovsky.schoolmanagerbackend.service.UsersService;
 
 @RestController
@@ -31,6 +30,7 @@ import pro.sorokovsky.schoolmanagerbackend.service.UsersService;
 public class UsersController {
     private final UsersService service;
     private final UserMapper mapper;
+    private final AuthorizationService authorizationService;
 
     @PutMapping
     @Operation(summary = "Оновлення користувача", description = "Користувач може оновити себе")
@@ -67,5 +67,30 @@ public class UsersController {
             @Valid @RequestBody UpdateUser updateUser
     ) {
         return ResponseEntity.ok(mapper.toGet(service.update(user, updateUser)));
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Видалення користувача")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Успішне видалення."
+            ),
+            @ApiResponse(
+                    responseCode = "401",
+                    description = "Неавторизований.",
+                    content = @Content(
+                            mediaType = MediaType.APPLICATION_JSON_VALUE,
+                            schema = @Schema(implementation = ProblemDetail.class)
+                    )
+            ),
+    })
+    public ResponseEntity<Void> delete(
+            @AuthenticationPrincipal UserEntity user,
+            HttpServletResponse response
+    ) {
+        service.delete(user.getId());
+        authorizationService.logout(response);
+        return ResponseEntity.noContent().build();
     }
 }
